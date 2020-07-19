2a1c69a07dc280d171122a5b4d3382e8e19cf11d
Cade Rosche <caderosche@gmail.com>
2020-07-18 21:02:19 -0500

    added scoffolding for hard stuff

---
+++ math-parser-js/script.js
@@ -0,0 +1,16 @@
+//Selectors
+const parseInput = document.getElementById("parse-input"); //gets the input box
+const parseButton = document.getElementById("parse-submit"); //gets the button
+const parseOutput = document.getElementById("parse-result"); //get the output element
+
+//event listeners
+parseButton.addEventListener("click", parse);
+
+//math parsing function
+//currently built to handle:
+//wont handle: + - / * () (yet)
+function parse(e) {
+  e.preventDefault();
+  let stringToParse = parseInput.value;
+  parseOutput.innerHTML = stringToParse;
+}
